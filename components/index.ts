667dd19eb83ad8230b2bd48b10d96dbd97931de4
garyMamaniCortez <mamanijhonny2210@gmail.com>
2024-11-13 15:20:23 -0400

    update exports components

--- components/index.ts
+++ components/index.ts
@@ -1,1 +1,3 @@
 export { default as ThemedText, ThemedTextProps } from './ThemedText/ThemedText'
+export { default as FoodCard } from './FoodCard/FoodCard';
+export { default as ThemedInput } from './ThemedInput/ThemedInput';
