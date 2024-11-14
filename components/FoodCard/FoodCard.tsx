90975e6f511c5632ddbcb54f2052896a46f71378
garyMamaniCortez <mamanijhonny2210@gmail.com>
2024-11-14 10:12:20 -0400

    refactor render stars is now a component

--- components/FoodCard/FoodCard.tsx
+++ components/FoodCard/FoodCard.tsx
@@ -1,32 +1,20 @@
 import React from 'react';
 import { View, Text, Image, StyleSheet } from 'react-native';
-import { FontAwesome } from '@expo/vector-icons';
 import styles from './styles';
 import { FoodCardProps } from '@/interfaces/FoodCardProps';
+import Stars from '../Stars/Stars';
 
 const FoodCard: React.FC<FoodCardProps> = ({ imageUrl, title, rating }) => {
-    const fullStars = Math.floor(rating);
-    const halfStar = rating % 1 >= 0.5;
-    const emptyStars = 5 - fullStars - (halfStar ? 1 : 0);
 
     return (
         <View style={styles.card}>
             <Image source={{ uri: imageUrl }} style={styles.image} />
             <View style={styles.infoContainer}>
                 <Text style={styles.title}>{title}</Text>
-                <View style={styles.ratingContainer}>
-                    {[...Array(fullStars)].map((_, i) => (
-                        <FontAwesome key={`full-${i}`} name="star" size={16} color="#FFD700" />
-                    ))}
-                    {halfStar && <FontAwesome name="star-half" size={16} color="#FFD700" />}
-                    {[...Array(emptyStars)].map((_, i) => (
-                        <FontAwesome key={`empty-${i}`} name="star-o" size={16} color="#FFD700" />
-                    ))}
-                </View>
+                <Stars rating={rating} />
             </View>
         </View>
     );
 };
-
 
 export default FoodCard;
