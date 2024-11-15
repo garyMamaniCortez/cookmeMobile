dc5e4cad3fe5073f90a0e1838fbc4c55cdf96726
garyMamaniCortez <mamanijhonny2210@gmail.com>
2024-11-15 12:33:51 -0400

    update components index exports

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,10 @@
+export { default as CommentBox} from './NotificationBox/NotificationBox';
+export { default as InfoBox } from './InfoBox/InfoBox';
+export { default as Navbar } from './Navbar/Navbar';
+export { default as NotificationBox } from './NotificationBox/NotificationBox';
+export { default as SearchBar } from './SearchBar/SearchBar';
+export { default as Stars } from './Stars/Stars';
+export { default as ThemedButton } from './ThemedButton/ThemedButton';
 export { default as ThemedText, ThemedTextProps } from './ThemedText/ThemedText'
 export { default as FoodCard } from './FoodCard/FoodCard';
 export { default as ThemedInput } from './ThemedInput/ThemedInput';
