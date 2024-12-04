c127b063d62a800a93f7c05fae64b03435b35643
garyMamaniCortez <mamanijhonny2210@gmail.com>
2024-12-03 21:33:58 -0400

    update the function to call the hook and render the response

--- src/SearchScreen/SearchScreen.tsx
+++ src/SearchScreen/SearchScreen.tsx
@@ -1,54 +1,63 @@
-import React from 'react';
-import { View, Text, TextInput, Image, TouchableOpacity, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, TouchableOpacity, ScrollView, Alert } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { StatusBar } from 'expo-status-bar';
 import { Navbar, RecipeCard } from '@/components';
 import { LinearGradient } from 'expo-linear-gradient';
-import { useRecetas } from '@/hooks/useReceta';
+import { useSearch } from '@/hooks/useReceta';
 import ErrorScreen from '../ErrorScreen/ErrorScreen';
 import styles from './styles';
+import { RecetaSearch } from '@/interfaces/api/Receta';
 
 const SearchScreen: React.FC = () => {
-  const {data, error} = useRecetas();
+  const [searchText, setSearchText] = useState('');
+  const [query, setQuery] = useState<RecetaSearch | null>(null);
+  const { data, error } = useSearch(query!);
 
-  if(data)
-    return (
-      <>
-        <LinearGradient
-          colors={['transparent', '#FB8B24']} // Fondo degradado de arriba hacia abajo
-          style={styles.container}
-        >
-          <StatusBar style="dark" backgroundColor="transparent" translucent={false} />
+  const handleSearch = () => {
+    setQuery({query:searchText});
+    setSearchText('');
+  };
 
-          {/* Header */}
-          <View style={styles.header}>
-            <TouchableOpacity>
-              <Ionicons name="menu" size={24} color="black" />
-            </TouchableOpacity>
-            <TextInput
-              style={styles.searchBar}
-              placeholder="Busqueda"
-              placeholderTextColor="#AAA" />
+  if (error) {
+    return <ErrorScreen error={error} />;
+  }
+
+  return (
+    <>
+      <LinearGradient
+        colors={['transparent', '#FB8B24']}
+        style={styles.container}
+      >
+        <StatusBar style="dark" backgroundColor="transparent" translucent={false} />
+
+        {/* Header */}
+        <View style={styles.header}>
+          <TouchableOpacity>
+            <Ionicons name="menu" size={24} color="black" />
+          </TouchableOpacity>
+          <TextInput
+            style={styles.searchBar}
+            placeholder="Búsqueda"
+            placeholderTextColor="#AAA"
+            value={searchText}
+            onChangeText={setSearchText} // Llama a la función handleSearch cuando cambia el texto
+            onSubmitEditing={handleSearch}
+          />
+        </View>
+
+        {/* Scrollable Content */}
+        <ScrollView contentContainerStyle={styles.contentContainer}>
+          <View style={styles.grid}>
+            {data && data.map((receta) => (
+              <RecipeCard key={receta.id_receta.toString()} receta={receta} />
+            ))}
           </View>
-
-          {/* Scrollable Content */}
-          <ScrollView contentContainerStyle={styles.contentContainer}>
-            {/* Smaller cards (with first card as button) */}
-            <View style={styles.grid}>
-              {/*Cards */}
-              {data.map((receta) => (
-                <RecipeCard key={receta.id_receta.toString()} receta={receta} />
-              ))}
-            </View>
-          </ScrollView>
-        </LinearGradient>
-        <Navbar />
-      </>
-    );
-  else
-    return (
-      <ErrorScreen error={error!} />
-    );
+        </ScrollView>
+      </LinearGradient>
+      <Navbar />
+    </>
+  );
 };
 
 export default SearchScreen;
